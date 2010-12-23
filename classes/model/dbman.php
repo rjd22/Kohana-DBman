f75545942fdcc76fb26384cb7e28acedd4b19107
Robert-Jan de Dreu <robertjandreu@hotmail.com>
2010-12-23 14:26:04 +0100

    Added license

--- classes/model/dbman.php
+++ classes/model/dbman.php
@@ -1,4 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+/**
+* Dbman
+*
+* @package        Dbman
+* @author         Robert-Jan de Dreu
+* @copyright      (c) 2010 Robert-Jan de Dreu
+* @license        http://www.opensource.org/licenses/isc-license.txt
+*/
 
 class Model_Dbman
 {
